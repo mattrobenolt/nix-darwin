0ed78eefd54fa8521ed8c8b3104c0030a37ec798
Matt Robenolt <m@robenolt.com>
2026-03-30 14:28:06 -0700

    use 1password agent on PC only when socket exists

--- hosts/nixos/nixos/home/ssh.nix
+++ hosts/nixos/nixos/home/ssh.nix
@@ -2,6 +2,10 @@ _: {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
+    extraConfig = ''
+      Match exec "test -S ~/.1password/agent.sock"
+        IdentityAgent ~/.1password/agent.sock
+    '';
 
     matchBlocks = {
       "github.com" = {
